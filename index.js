a990090ffefa935d2f1337733de99021fdb222d1
Taras Tereschenko <Taras_Tereschenko@epam.com>
2023-02-25 23:34:49 +0500

    add touch support

--- index.js
+++ index.js
@@ -1,92 +1,103 @@
 const gameField = document.querySelector("#game-field");
 const ball = document.querySelector("#ball");
 
-const pressedKeys = new Set();
+let degrees = null;
+
+let pressedKeys = new Set();
+
+const directions = {
+	up: ["w", "ArrowUp"],
+	down: ["s", "ArrowDown"],
+	left: ["a", "ArrowLeft"],
+	right: ["d", "ArrowRight"],
+	upLeft: ["w", "ArrowUp", "a", "ArrowLeft"],
+	upRight: ["w", "ArrowUp", "d", "ArrowRight"],
+	downLeft: ["s", "ArrowDown", "a", "ArrowLeft"],
+	downRight: ["s", "ArrowDown", "d", "ArrowRight"],
+};
+
+const getDegreesFromPressedKeys = () => {
+	if (pressedKeys.size >= 2) {
+		const lastTwoKeys = [...pressedKeys].slice(-2);
+		if (lastTwoKeys.every((key) => directions.upLeft.includes(key)))
+			return 135;
+		if (lastTwoKeys.every((key) => directions.upRight.includes(key)))
+			return 45;
+		if (lastTwoKeys.every((key) => directions.downLeft.includes(key)))
+			return 225;
+		if (lastTwoKeys.every((key) => directions.downRight.includes(key)))
+			return 315;
+	} else if (pressedKeys.size === 1) {
+		const key = [...pressedKeys][0];
+		if (directions.up.includes(key)) return 90;
+		if (directions.down.includes(key)) return 270;
+		if (directions.left.includes(key)) return 180;
+		if (directions.right.includes(key)) return 0;
+	}
+	return null;
+};
 
 document.addEventListener("keydown", (event) => {
 	pressedKeys.add(event.key);
+	degrees = getDegreesFromPressedKeys();
 });
 
 document.addEventListener("keyup", (event) => {
 	pressedKeys.delete(event.key);
+	if (pressedKeys.size === 0) degrees = null;
 });
 
-const directions = {
-	up: "up",
-	down: "down",
-	left: "left",
-	right: "right",
-};
+let lastTouchCoordinates = null;
 
-const moveKeys = {
-	[directions.up]: ["w", "ArrowUp"],
-	[directions.down]: ["s", "ArrowDown"],
-	[directions.left]: ["a", "ArrowLeft"],
-	[directions.right]: ["d", "ArrowRight"],
-};
+document.addEventListener("touchstart", (event) => {
+	const { clientX, clientY } = event.touches[0];
+	lastTouchCoordinates = { x: clientX, y: clientY };
+});
 
-const isMooving = (direction) => {
-	return moveKeys[direction].some((key) => pressedKeys.has(key));
-};
+document.addEventListener("touchmove", (event) => {
+	const { clientX, clientY } = event.touches[0];
 
-const canMove = (object, distance, direction) => {
+	if (lastTouchCoordinates !== null) {
+		const { x, y } = lastTouchCoordinates;
+		degrees = Math.atan2(y - clientY, clientX - x) * (180 / Math.PI);
+	}
+
+	lastTouchCoordinates = { x: clientX, y: clientY };
+});
+
+document.addEventListener("touchend", (event) => {
+	lastTouchCoordinates = null;
+	degrees = null;
+});
+
+const canMove = (object, distance, degrees) => {
 	const { top, left, bottom, right } = object.getBoundingClientRect();
 	const field = gameField.getBoundingClientRect();
 
-	switch (direction) {
-		case directions.up:
-			return top - distance > field.top;
-		case directions.down:
-			return bottom + distance < field.bottom;
-		case directions.left:
-			return left - distance > field.left;
-		case directions.right:
-			return right + distance < field.right;
-	}
+	const radians = (degrees * Math.PI) / 180;
+	const nextTop = top - distance * Math.sin(radians);
+	const nextLeft = left + distance * Math.cos(radians);
+	const nextBottom = bottom - distance * Math.sin(radians);
+	const nextRight = right + distance * Math.cos(radians);
+
+	return (
+		nextTop >= field.top &&
+		nextLeft >= field.left &&
+		nextBottom <= field.bottom &&
+		nextRight <= field.right
+	);
 };
 
-const moveObject = (object, distance, direction) => {
-	switch (direction) {
-		case directions.up:
-			object.style.top = `${object.offsetTop - distance}px`;
-			break;
-		case directions.down:
-			object.style.top = `${object.offsetTop + distance}px`;
-			break;
-		case directions.left:
-			object.style.left = `${object.offsetLeft - distance}px`;
-			break;
-		case directions.right:
-			object.style.left = `${object.offsetLeft + distance}px`;
-			break;
-	}
+const moveObject = (object, distance, degrees) => {
+	const radians = (degrees * Math.PI) / 180;
+	object.style.top = `${object.offsetTop - distance * Math.sin(radians)}px`;
+	object.style.left = `${object.offsetLeft + distance * Math.cos(radians)}px`;
 };
 
 const moveBall = (distance) => {
-	if (isMooving(directions.up) && canMove(ball, distance, directions.up)) {
-		moveObject(ball, distance, directions.up);
-	}
+	if (degrees === null || !canMove(ball, distance, degrees)) return;
 
-	if (
-		isMooving(directions.down) &&
-		canMove(ball, distance, directions.down)
-	) {
-		moveObject(ball, distance, directions.down);
-	}
-
-	if (
-		isMooving(directions.left) &&
-		canMove(ball, distance, directions.left)
-	) {
-		moveObject(ball, distance, directions.left);
-	}
-
-	if (
-		isMooving(directions.right) &&
-		canMove(ball, distance, directions.right)
-	) {
-		moveObject(ball, distance, directions.right);
-	}
+	moveObject(ball, distance, degrees);
 };
 
 const renderFadingTailCell = (ballSpeed) => {
