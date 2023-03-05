417a8e78eb7952272b14f0238962476a29b3ea41
Taras Tereschenko <Taras_Tereschenko@epam.com>
2023-03-05 22:34:47 +0500

    add beta mobile support

--- player.js
+++ player.js
@@ -11,6 +11,7 @@ export class Player {
 
 	segmentsNumber = 5
 	segments = []
+	lastTouchCoordinates = null
 
 	constructor(fieldSize) {
 		this.fieldSize = fieldSize
@@ -23,6 +24,12 @@ export class Player {
 		}
 
 		document.addEventListener('keydown', this.onKeyDown.bind(this))
+
+		// [BETA] mobile support (touch events)
+		// TODO: refactor
+		document.addEventListener('touchstart', this.onTouchStart.bind(this))
+		document.addEventListener('touchmove', this.onTouchMove.bind(this))
+		document.addEventListener('touchend', this.onTouchEnd.bind(this))
 	}
 
 	getCorrectCoordinates(x, y) {
@@ -84,4 +91,38 @@ export class Player {
 		}
 	}
 
+	onTouchStart(event) {
+		const { clientX, clientY } = event.touches[0]
+		this.lastTouchCoordinates = { x: clientX, y: clientY }
+	}
+
+	onTouchMove(event) {
+		if (!this.lastTouchCoordinates) return
+
+		const { clientX, clientY } = event.touches[0]
+		const { x: lastX, y: lastY } = this.lastTouchCoordinates
+
+		const xDiff = clientX - lastX
+		const yDiff = clientY - lastY
+
+		if (Math.abs(xDiff) > Math.abs(yDiff)) {
+			if (xDiff > 0) {
+				this.move(directions.RIGHT)
+			} else {
+				this.move(directions.LEFT)
+			}
+		} else {
+			if (yDiff > 0) {
+				this.move(directions.DOWN)
+			} else {
+				this.move(directions.UP)
+			}
+		}
+
+		this.lastTouchCoordinates = { x: clientX, y: clientY }
+	}
+
+	onTouchEnd() {
+		this.lastTouchCoordinates = null
+	}
 }
